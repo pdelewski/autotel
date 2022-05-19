6336ff33a7c5ce614c2f5fdfafcebcf68cdf322e
Przemek Delewski <pdelewski@sumologic.com>
2022-05-19 22:32:46 +0200

    send fake post to service

--- service/main.go
+++ service/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func inject(w http.ResponseWriter, r *http.Request) {
+	var bodyBytes []byte
+	var err error
+
+	if r.Body != nil {
+		bodyBytes, err = ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Printf("Body reading error: %v", err)
+			return
+		}
+		defer r.Body.Close()
+	}
+
+	if len(bodyBytes) > 0 {
+		fmt.Println(string(bodyBytes))
+	} else {
+		fmt.Printf("Body: No Body Supplied\n")
+	}
+	fmt.Fprintf(w, "inject\n")
+
+}
+
 func main() {
+
+	http.HandleFunc("/inject", inject)
+
+	http.ListenAndServe(":8090", nil)
 }
