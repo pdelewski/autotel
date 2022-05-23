169f5d6ca27b84f54c30c594a13de8c61a91639e
Przemek Delewski <pdelewski@sumologic.com>
2022-05-23 20:18:29 +0200

    children/successors selection

--- service/static/callgraph.js
+++ service/static/callgraph.js
@@ -1,10 +1,10 @@
 var callgraph = {
 	nodes: [
+		 { data: { id: 'main' } },
 		 { data: { id: 'Fibonacci' } },
 		 { data: { id: 'FibonacciHelper' } },
 		 { data: { id: 'uint64' } },
 		 { data: { id: 'uint' } },
-		 { data: { id: 'main' } },
 	],
 	edges: [
 		 { data: { id: 'e0', source: 'Fibonacci', target: 'uint64' } },
