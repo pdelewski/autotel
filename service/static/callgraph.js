2ba0caac9248e5e9dfa82e673c290c932276c779
Przemek Delewski <pdelewski@sumologic.com>
2022-05-23 12:57:11 +0200

    build graph

--- service/static/callgraph.js
+++ service/static/callgraph.js
@@ -1,15 +1,15 @@
 var callgraph = {
 	nodes: [
-		 { data: { id: 'Fibonacci' } },
 		 { data: { id: 'FibonacciHelper' } },
 		 { data: { id: 'uint64' } },
 		 { data: { id: 'uint' } },
 		 { data: { id: 'main' } },
+		 { data: { id: 'Fibonacci' } },
 	],
 	edges: [
-		 { data: { id: 'e0', source: 'Fibonacci', target: 'uint64' } },
-		 { data: { id: 'e1', source: 'Fibonacci', target: 'uint' } },
-		 { data: { id: 'e2', source: 'main', target: 'FibonacciHelper' } },
-		 { data: { id: 'e3', source: 'FibonacciHelper', target: 'Fibonacci' } },
+		 { data: { id: 'e0', source: 'Fibonacci', target: 'uint' } },
+		 { data: { id: 'e1', source: 'main', target: 'FibonacciHelper' } },
+		 { data: { id: 'e2', source: 'FibonacciHelper', target: 'Fibonacci' } },
+		 { data: { id: 'e3', source: 'Fibonacci', target: 'uint64' } },
 	]
 };
