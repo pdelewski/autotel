56a7ecbb14ad667bd3df5adeaa382f0e3bdf4bd6
Przemek Delewski <pdelewski@sumologic.com>
2022-06-07 15:59:22 +0200

    output context propagation pass

--- autotellib/context_propagation.go
+++ autotellib/context_propagation.go
@@ -1,9 +1,12 @@
 package autotellib
 
 import (
+	"fmt"
 	"go/ast"
 	"go/parser"
+	"go/printer"
 	"go/token"
+	"os"
 
 	"golang.org/x/tools/go/ast/astutil"
 )
@@ -16,6 +19,31 @@ func PropagateContext(file string, callgraph map[string]string, rootFunctions []
 	}
 	astutil.AddImport(fset, node, "context")
 	ast.Inspect(node, func(n ast.Node) bool {
+		switch x := n.(type) {
+		case *ast.FuncDecl:
+			// inject context only
+			// functions available in the call graph
+			_, exists := callgraph[x.Name.Name]
+			if !exists {
+				return false
+			}
+			fmt.Printf("function decl: %s, parameters:\n", x.Name)
+			for _, param := range x.Type.Params.List {
+				fmt.Printf("  Name: %s\n", param.Names[0])
+				fmt.Printf("    ast type          : %T\n", param.Type)
+				fmt.Printf("    type desc         : %+v\n", param.Type)
+			}
+
+		case *ast.CallExpr:
+			ident, ok := x.Fun.(*ast.Ident)
+			if ok {
+				fmt.Println("call:", ident.Name)
+			}
+		}
 		return true
 	})
+	out, err := os.Create(file + ".pass_ctx")
+	defer out.Close()
+
+	printer.Fprint(out, fset, node)
 }
