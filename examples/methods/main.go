e29d224a6ddc43a00631189cc7ef00fbbf657620
Przemek Delewski <pdelewski@sumologic.com>
2022-06-11 22:03:33 +0200

    struct composite

--- examples/methods/main.go
+++ examples/methods/main.go
@@ -2,14 +2,22 @@ package main
 
 import "sumologic.com/autotel/rtlib"
 
+type element struct {
+}
+
 type driver struct {
+  e element
 }
 
 func (d driver) process(a int) {
+}
+
+func (e element) get(a int) {
 }
 
 func main() {
 	rtlib.SumoAutoInstrument()
 	d := driver{}
 	d.process(10)
+	d.e.get(5)
 }
