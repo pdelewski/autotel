a0460ac1cc6767cb3d5d845a4100869ccfcba3a5
Przemek Delewski <pdelewski@sumologic.com>
2022-06-22 15:23:33 +0200

    cleanup

--- autotellib/callgraph.go
+++ autotellib/callgraph.go
@@ -3,7 +3,6 @@ package autotellib
 import (
 	"fmt"
 	"go/ast"
-	"go/parser"
 	"go/token"
 	"log"
 	"os"
@@ -55,84 +54,6 @@ func GlobalFindRootFunctions(projectPath string, packagePattern string) []string
 		}
 	}
 	return rootFunctions
-}
-
-func FindRootFunctions(file string) []string {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, file, nil, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
-
-	var currentFun string
-	var rootFunctions []string
-
-	ast.Inspect(node, func(n ast.Node) bool {
-		switch x := n.(type) {
-		case *ast.CallExpr:
-			_, ok := x.Fun.(*ast.Ident)
-			if ok {
-			}
-			selector, ok := x.Fun.(*ast.SelectorExpr)
-			if ok {
-				if selector.Sel.Name == "SumoAutoInstrument" {
-					rootFunctions = append(rootFunctions, currentFun)
-				}
-			}
-		case *ast.FuncDecl:
-			currentFun = x.Name.Name
-		}
-		return true
-	})
-
-	return rootFunctions
-}
-
-func BuildCallGraph(file string) map[string][]string {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, file, nil, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
-
-	currentFun := "nil"
-	backwardCallGraph := make(map[string][]string)
-
-	ast.Inspect(node, func(n ast.Node) bool {
-		switch x := n.(type) {
-		case *ast.CallExpr:
-			id, ok := x.Fun.(*ast.Ident)
-			if ok {
-				if !Contains(backwardCallGraph[id.Name], currentFun) {
-					backwardCallGraph[id.Name] = append(backwardCallGraph[id.Name], currentFun)
-				}
-			}
-			sel, ok := x.Fun.(*ast.SelectorExpr)
-			if ok {
-				if !Contains(backwardCallGraph[sel.Sel.Name], currentFun) {
-					backwardCallGraph[sel.Sel.Name] = append(backwardCallGraph[sel.Sel.Name], currentFun)
-				}
-			}
-		case *ast.FuncDecl:
-			currentFun = x.Name.Name
-		}
-		return true
-	})
-
-	return backwardCallGraph
-}
-
-func BuildCompleteCallGraph(files []string, funcDecls map[string]bool) map[string][]string {
-	backwardCallGraph := make(map[string][]string)
-	for _, file := range files {
-		callGraphInstance := BuildCallGraph(file)
-		for key, funList := range callGraphInstance {
-			if funcDecls[key] == true {
-				backwardCallGraph[key] = append(backwardCallGraph[key], funList...)
-			}
-		}
-	}
-	return backwardCallGraph
 }
 
 func GlobalBuildCallGraph(projectPath string, packagePattern string, funcDecls map[string]bool) map[string][]string {
@@ -198,37 +119,6 @@ func GlobalFindFuncDecls(projectPath string, packagePattern string) map[string]b
 				}
 				return true
 			})
-		}
-	}
-	return funcDecls
-}
-
-func FindFuncDecls(file string) map[string]bool {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, file, nil, parser.ParseComments)
-	if err != nil {
-		panic(err)
-	}
-
-	funcDecls := make(map[string]bool)
-
-	ast.Inspect(node, func(n ast.Node) bool {
-		switch x := n.(type) {
-		case *ast.FuncDecl:
-			funcDecls[x.Name.Name] = true
-		}
-		return true
-	})
-
-	return funcDecls
-}
-
-func FindCompleteFuncDecls(files []string) map[string]bool {
-	funcDecls := make(map[string]bool)
-	for _, file := range files {
-		funcDeclsFile := FindFuncDecls(file)
-		for k, v := range funcDeclsFile {
-			funcDecls[k] = v
 		}
 	}
 	return funcDecls
--- autotellib/context_propagation.go
+++ autotellib/context_propagation.go
@@ -3,7 +3,6 @@ package autotellib
 import (
 	"fmt"
 	"go/ast"
-	"go/parser"
 	"go/printer"
 	"go/token"
 	"log"
@@ -12,112 +11,6 @@ import (
 	"golang.org/x/tools/go/ast/astutil"
 	"golang.org/x/tools/go/packages"
 )
-
-func PropagateContext(file string, callgraph map[string][]string, rootFunctions []string, funcDecls map[string]bool, passFileSuffix string) {
-	fset := token.NewFileSet()
-	node, err := parser.ParseFile(fset, file, nil, parser.AllErrors)
-	if err != nil {
-		panic(err)
-	}
-	out, _ := os.Create(file + passFileSuffix)
-	defer out.Close()
-
-	if len(rootFunctions) == 0 {
-		printer.Fprint(out, fset, node)
-		return
-	}
-
-	astutil.AddImport(fset, node, "context")
-
-	ast.Inspect(node, func(n ast.Node) bool {
-		ctxArg := &ast.Ident{
-			Name: "__child_tracing_ctx",
-		}
-		ctxField := &ast.Field{
-			Names: []*ast.Ident{
-				&ast.Ident{
-					Name: "__tracing_ctx",
-				},
-			},
-			Type: &ast.SelectorExpr{
-				X: &ast.Ident{
-					Name: "context",
-				},
-				Sel: &ast.Ident{
-					Name: "Context",
-				},
-			},
-		}
-		switch x := n.(type) {
-		case *ast.FuncDecl:
-			// inject context only
-			// functions available in the call graph
-			// _, exists := callgraph[x.Name.Name]
-			// if !exists {
-			// 	return false
-			// }
-			// TODO this is not optimap o(n)
-			exists := false
-			for k, v := range callgraph {
-				if k == x.Name.Name {
-					exists = true
-				}
-				for _, e := range v {
-					if x.Name.Name == e {
-						exists = true
-					}
-				}
-			}
-			if !exists {
-				break
-			}
-
-			if Contains(rootFunctions, x.Name.Name) {
-				break
-			}
-			// fmt.Printf("function decl: %s, parameters:\n", x.Name)
-			// for _, param := range x.Type.Params.List {
-			// fmt.Printf("  Name: %s\n", param.Names[0])
-			// fmt.Printf("    ast type          : %T\n", param.Type)
-			// fmt.Printf("    type desc         : %+v\n", param.Type)
-			// }
-			x.Type.Params.List = append(x.Type.Params.List, ctxField)
-		case *ast.CallExpr:
-			ident, ok := x.Fun.(*ast.Ident)
-			if ok {
-				found := funcDecls[ident.Name]
-				_ = found
-				// inject context parameter only
-				// to these functions for which function decl
-				// exists
-
-				if found {
-					x.Args = append(x.Args, ctxArg)
-				}
-			}
-			_, ok = x.Fun.(*ast.FuncLit)
-			if ok {
-				x.Args = append(x.Args, ctxArg)
-			}
-			// TODO selectors are recursive
-			// to handle a.b.c.fun()
-			// all selectors have to unpacked
-			sel, ok := x.Fun.(*ast.SelectorExpr)
-			if ok {
-				// packageIdent, ok := sel.X.(*ast.Ident)
-				found := funcDecls[sel.Sel.Name]
-				if found {
-					x.Args = append(x.Args, ctxArg)
-				}
-			}
-		case *ast.FuncLit:
-			x.Type.Params.List = append(x.Type.Params.List, ctxField)
-		}
-		return true
-	})
-
-	printer.Fprint(out, fset, node)
-}
 
 func GlobalPropagateContext(projectPath string, packagePattern string, callgraph map[string][]string, rootFunctions []string, funcDecls map[string]bool, passFileSuffix string) {
 	fset := token.NewFileSet()
